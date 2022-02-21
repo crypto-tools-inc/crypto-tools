a69369da40c8953cecbe66a877d48c501ce6577d
FabioMonzani <renderbros@gmail.com>
2022-02-21 15:21:22 +0000

    Update pages, data and img structure

--- js/get-content.js
+++ js/get-content.js
@@ -1,21 +1,23 @@
 function getContent() {
   var pageName = $('#section-elements').attr('name');
+  var toolCount = $('#tool-count');
   var loadFile = "data/" + pageName + ".json";
 
-  $.ajaxSetup();
+  $.ajaxSetup({ cache: false });//Always get the latest version
   // FETCHING DATA FROM JSON FILE
   $.getJSON(loadFile, function (data) {
     var item = '';
     // ITERATING THROUGH OBJECTS
     $.each(data, function (key, value) {
+      //Get the total number of tools in this section
+      toolCount.text(data.length + ' Tools');
 
       //CONSTRUCTION OF ROWS HAVING
       // DATA FROM JSON OBJECT
-      // console.log(value.name);
       item += '<div class="col-xl-3 col-lg-4 col-md-6 col-12">';
       item += '<div class="card">';
       item += '<div class="card-header d-flex">';
-      item += '<img loading="lazy" src="img/' + value.logo + '" height="70" class="rounded-circle" alt="' + value.logo + '">';
+      item += '<img loading="lazy" src="img/projects/' + value.logo + '" height="70" class="rounded-circle" alt="' + value.logo + '">';
       item += '<div class="ms-4 d-flex flex-column justify-content-between">';
       item += '<h4 class="card-title">' + value.name + '</h4>';
       item += '<a class="card-subtitle mb-2 text-decoration-none" href="' + value.website + '" target="_blank"><img class="me-1" height="12" src="img/socials/link.svg">Website</a>';
@@ -25,9 +27,7 @@ function getContent() {
       item += '<p class="text-muted text-uppercase small semi-bold mb-2">Socials</p>';
       item += '<ul class="list-inline m-0">';
       $.each(value.socials, function (key, socials) {
-        // console.log(socials);
         if (socials == "") {
-          // console.log(key + ' is empty')
         } else {
           item += '<li class="list-inline-item"><img class="me-1" height="12" src="img/socials/' + key +'.svg"><a class="text-capitalize me-4 text-decoration-none" href="' + socials + '" target="_blank">'+ key + '</a></li>';
         }
@@ -37,7 +37,6 @@ function getContent() {
       item += '<div class="card-footer">';
       item += '<p class="text-muted text-uppercase small semi-bold mb-2">Networks</p>';
       $.each(value.network, function (key, network) {
-        // console.log(network);
         item += '<span class="badge bg-label me-2 text-capitalize"><img class="me-1" height="12" src="img/networks/' + network +'.svg">' + network + '</span>';
       })
       item += '</div></div></div>';
