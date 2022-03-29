30b0ee19061b7f777aa07fa7153cd08b984502b0
FabioMonzani <renderbros@gmail.com>
2022-03-29 23:00:30 +0100

    Fixed relative path

--- js/get-content.js
+++ js/get-content.js
@@ -1,7 +1,7 @@
 function getContent() {
   var pageName = $('#section-elements').attr('name');
   var toolCount = $('#tool-count');
-  var loadFile = "/data/" + pageName + ".json";
+  var loadFile = "./data/" + pageName + ".json";
 
   $.ajaxSetup({ cache: false });//Always get the latest version
   // FETCHING DATA FROM JSON FILE
@@ -17,10 +17,10 @@ function getContent() {
       item += '<div class="col-xxl-3 col-xl-3 col-lg-4 col-md-6 col-12">';
       item += '<div class="card">';
       item += '<div class="card-header d-flex">';
-      item += '<img loading="lazy" src="/img/projects/' + value.logo + '" height="70" class="rounded-circle" alt="' + value.logo + '">';
+      item += '<img loading="lazy" src="./img/projects/' + value.logo + '" height="70" class="rounded-circle" alt="' + value.logo + '">';
       item += '<div class="ms-4 d-flex flex-column justify-content-between">';
       item += '<h4 class="card-title">' + value.name + '</h4>';
-      item += '<a class="card-subtitle mb-2 text-decoration-none" href="' + value.website + '" target="_blank"><img class="me-1" height="12" src="/img/socials/link.svg">Website</a>';
+      item += '<a class="card-subtitle mb-2 text-decoration-none" href="' + value.website + '" target="_blank"><img class="me-1" height="12" src="./img/socials/link.svg">Website</a>';
       item += '</div></div>';
       item += '<div class="card-body"><div>';
       item += '<p class="card-text">' + value.description + '</p>';
@@ -29,7 +29,7 @@ function getContent() {
       $.each(value.socials, function (key, socials) {
         if (socials == "") {
         } else {
-          item += '<li class="list-inline-item"><img class="me-1" height="12" src="/img/socials/' + key +'.svg"><a class="text-capitalize me-4 text-decoration-none" href="' + socials + '" target="_blank">'+ key + '</a></li>';
+          item += '<li class="list-inline-item"><img class="me-1" height="12" src="./img/socials/' + key +'.svg"><a class="text-capitalize me-4 text-decoration-none" href="' + socials + '" target="_blank">'+ key + '</a></li>';
         }
       })
       item += '<ul>';
@@ -37,7 +37,7 @@ function getContent() {
       item += '<div class="card-footer">';
       item += '<p class="text-muted text-uppercase small semi-bold mb-2">Networks</p>';
       $.each(value.network, function (key, network) {
-        item += '<span class="badge bg-label me-2 text-capitalize"><img class="me-1" height="12" src="/img/networks/' + network +'.svg">' + network + '</span>';
+        item += '<span class="badge bg-label me-2 text-capitalize"><img class="me-1" height="12" src="./img/networks/' + network +'.svg">' + network + '</span>';
       })
       item += '</div></div></div>';
     });
