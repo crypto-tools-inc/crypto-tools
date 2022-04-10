faa09a8f336c1ac9c1b000f5b16d051a13f872e5
FabioMonzani <renderbros@gmail.com>
2022-04-10 21:48:52 +0100

    If socials are empty do not display

--- js/get-content.js
+++ js/get-content.js
@@ -24,14 +24,17 @@ function getContent() {
       item += '</div></div>';
       item += '<div class="card-body"><div>';
       item += '<p class="card-text">' + value.description + '</p>';
-      item += '<p class="text-muted text-uppercase small semi-bold mb-2">Socials</p>';
-      item += '<ul class="list-inline m-0">';
-      $.each(value.socials, function (key, socials) {
-        if (socials == "") {
-        } else {
-          item += '<li class="list-inline-item"><img class="me-1" height="12" src="/img/socials/' + key +'.svg"><a class="text-capitalize me-4 text-decoration-none" href="' + socials + '" target="_blank">'+ key + '</a></li>';
-        }
-      })
+      if ($.isEmptyObject(value.socials)) {
+      } else {
+        item += '<p class="text-muted text-uppercase small semi-bold mb-2">Socials</p>';
+        item += '<ul class="list-inline m-0">';
+        $.each(value.socials, function (key, socials) {
+          if (socials == "") {
+          } else {
+            item += '<li class="list-inline-item"><img class="me-1" height="12" src="/img/socials/' + key +'.svg"><a class="text-capitalize me-4 text-decoration-none" href="' + socials + '" target="_blank">'+ key + '</a></li>';
+          }
+        })
+      }
       item += '<ul>';
       item += '</div></div>';
       item += '<div class="card-footer">';
