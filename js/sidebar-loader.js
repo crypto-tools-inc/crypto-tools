8e4f571abb4e0d382b12755f514b19192b988534
FabioMonzani <renderbros@gmail.com>
2022-09-19 00:07:09 +0100

    Navbar include project request

--- js/sidebar-loader.js
+++ js/sidebar-loader.js
@@ -1,4 +1,5 @@
   $('#sidebar-wrapper').load('../include/sidebar.html');
+  $('#main-navbar').load('../include/navbar.html');
   var pageTitle = $('title').text();
   $('#section-title').text(pageTitle);
 $(document).ready(function () {
