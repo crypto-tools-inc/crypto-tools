8817bf3d46e20938a1c345941f82a311551cab7c
FabioMonzani <renderbros@gmail.com>
2023-05-19 07:56:39 +0100

    jquery to JS conversion

--- js/search-content.js
+++ js/search-content.js
@@ -1,26 +1,55 @@
-    $(document).keydown(function() {
-        if (event.keyCode == 191) {
-            $('#searchContent').focus();
-            return false;
-        }
-    })
-    function searchContent() {
-      // Declare variables
-      var input, filter, ul, li, a, i;
-      input = document.getElementById('searchContent');
-      filter = input.value.toLowerCase();
-      ul = document.getElementById("section-elements");
-      li = ul.getElementsByClassName('card');
+// JQuery
+// $(document).keydown(function () {
+//   if (event.keyCode == 191) {
+//     $("#searchContent").focus();
+//     return false;
+//   }
+// });
+// function searchContent() {
+//   // Declare variables
+//   var input, filter, ul, li, a, i;
+//   input = document.getElementById("searchContent");
+//   filter = input.value.toLowerCase();
+//   ul = document.getElementById("section-elements");
+//   li = ul.getElementsByClassName("card");
 
-      // Loop through all list items, and hide those who don't match the search query
-      for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByClassName("card-title")[0];
-          b = li[i].getElementsByClassName("card-text")[0];
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < li.length; i++) {
+//     a = li[i].getElementsByClassName("card-title")[0];
+//     b = li[i].getElementsByClassName("card-text")[0];
 
-          if (a.innerHTML.toLowerCase().indexOf(filter) > -1 || b.innerHTML.toLowerCase().indexOf(filter) > -1) {
-              li[i].parentElement.style.display = "";
-          } else {
-              li[i].parentElement.style.display = "none";
-          };
-      }
+//     if (a.innerHTML.toLowerCase().indexOf(filter) > -1 || b.innerHTML.toLowerCase().indexOf(filter) > -1) {
+//       li[i].parentElement.style.display = "";
+//     } else {
+//       li[i].parentElement.style.display = "none";
+//     }
+//   }
+// }
+
+// Vanilla JS
+document.addEventListener("keydown", function () {
+  if (event.keyCode === 191) {
+    document.getElementById("searchContent").focus();
+    return false;
+  }
+});
+
+function searchContent() {
+  // Declare variables
+  var input = document.getElementById("searchContent");
+  var filter = input.value.toLowerCase();
+  var ul = document.getElementById("section-elements");
+  var li = ul.getElementsByClassName("card");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (var i = 0; i < li.length; i++) {
+    var a = li[i].getElementsByClassName("card-title")[0];
+    var b = li[i].getElementsByClassName("card-text")[0];
+
+    if (a.innerHTML.toLowerCase().indexOf(filter) > -1 || b.innerHTML.toLowerCase().indexOf(filter) > -1) {
+      li[i].parentElement.style.display = "";
+    } else {
+      li[i].parentElement.style.display = "none";
     }
+  }
+}
