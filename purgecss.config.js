18cf546cd5a87651f52569ecf8230c02c4a01c31
FabioMonzani <renderbros@gmail.com>
2023-05-22 16:24:49 +0100

    Config format

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  content: [
-    '*.html',
-    'pages/*.html',
-    'include/*.html',
-    'js/*.js'
-  ],
-  css: [
-    'vendors/bootstrap-5.1.3/css/bootstrap.min.css',
-  ]
-}
+  content: ["*.html", "pages/*.html", "include/*.html", "js/*.js"],
+  css: ["vendors/bootstrap-5.3.0/css/bootstrap.css"],
+};
