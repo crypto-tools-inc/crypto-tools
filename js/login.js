cca53c89a8ecccec131da619c55f373f12d002e8
monzanifabio <renderbros@gmail.com>
2023-06-27 09:48:51 +0100

    Disable login button on login

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", async (event) => {
   event.preventDefault();
+  loginBtn.disabled = true;
   const isEmailValid = isValidEmail(userEmail.value);
   const isPasswordValid = isValidPassword(userPassword.value);
   if (isEmailValid) {
@@ -22,15 +23,19 @@ loginBtn.addEventListener("click", async (event) => {
       if (error) {
         document.getElementById("loginHelper").innerHTML = error;
         console.log(error);
+        loginBtn.disabled = false;
       } else {
         document.getElementById("loginHelper").innerHTML = "";
         console.log(data);
         window.location.replace("/index.html");
+        loginBtn.disabled = false;
       }
     } else {
       console.log("not a valid password");
+      loginBtn.disabled = false;
     }
   } else {
     console.log("not a valid email address.");
+    loginBtn.disabled = false;
   }
 });
