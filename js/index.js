22bd6fac784fc20457ebd4d32f5c69a249bb9778
monzanifabio <renderbros@gmail.com>
2023-06-27 09:19:20 +0100

    Updated the total count of tools from supabase

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let client = supabase.createClient("https://krperkqbaqewikgzuoea.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtycGVya3FiYXFld2lrZ3p1b2VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzMzU4NzcsImV4cCI6MTk5NTkxMTg3N30.ZiwrLZyY8lHlLspcVIagKrF5Bdci_R95lKpDDK56xHM");
+let totalCount = document.getElementById("totalCount");
+
+(async function () {
+  let { data, error } = await client.from("tools").select("*");
+  if (data) {
+    let total = data.length;
+    totalCount.textContent = total;
+  }
+})();
