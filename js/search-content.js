daf7a55cea7e3e01cafb885138919052cb46beb8
monzanifabio <renderbros@gmail.com>
2023-06-29 15:31:40 +0100

    Removed unused scripts

--- js/search-content.js
+++ js/search-content.js
@@ -1,31 +1,3 @@
-// JQuery
-// $(document).keydown(function () {
-//   if (event.keyCode == 191) {
-//     $("#searchContent").focus();
-//     return false;
-//   }
-// });
-// function searchContent() {
-//   // Declare variables
-//   var input, filter, ul, li, a, i;
-//   input = document.getElementById("searchContent");
-//   filter = input.value.toLowerCase();
-//   ul = document.getElementById("section-elements");
-//   li = ul.getElementsByClassName("card");
-
-//   // Loop through all list items, and hide those who don't match the search query
-//   for (i = 0; i < li.length; i++) {
-//     a = li[i].getElementsByClassName("card-title")[0];
-//     b = li[i].getElementsByClassName("card-text")[0];
-
-//     if (a.innerHTML.toLowerCase().indexOf(filter) > -1 || b.innerHTML.toLowerCase().indexOf(filter) > -1) {
-//       li[i].parentElement.style.display = "";
-//     } else {
-//       li[i].parentElement.style.display = "none";
-//     }
-//   }
-// }
-
 // Vanilla JS
 document.addEventListener("keydown", function () {
   if (event.keyCode === 191) {
