a2576cdaf463d380cba0f08430f09482a5a766dd
monzanifabio <renderbros@gmail.com>
2024-01-05 01:23:47 +0000

    Improved upvote logic

--- js/upvote.js
+++ js/upvote.js
@@ -11,33 +11,27 @@ async function upvote(el) {
     if (el.classList.contains("badge-active")) {
       const { data, error } = await client.from("upvotes").delete().eq("tool_id", el.id).select();
       if (data) {
-        console.log(data);
+        // console.log(data);
         el.classList.remove("badge-active");
         let newUpvotes = parseInt(currentUpvotes) - 1;
         el.innerHTML = `<img class="me-2" src="/img/caret-up.svg" width="14" height="14" alt="caret">` + newUpvotes;
       }
     } else {
-      el.classList.add("badge-active");
-      let newUpvotes = parseInt(currentUpvotes) + 1;
-
-      const { data, error } = await client.from("tools").update({ upvotes: newUpvotes }).eq("id", toolId).select();
+      const { data, error } = await client
+        .from("upvotes")
+        .insert([{ user_id: user_id, tool_id: toolId, user_email: user_email }])
+        .select();
       if (data) {
+        el.classList.add("badge-active");
+        let newUpvotes = parseInt(currentUpvotes) + 1;
         el.innerHTML = `<img class="me-2" src="/img/caret-up.svg" width="14" height="14" alt="caret">` + newUpvotes;
-        addUpvote(toolId);
+        // Update the upvote count in the tools table
+        const { data, error } = await client.from("tools").update({ upvotes: newUpvotes }).eq("id", toolId).select();
+        // console.log(data);
       }
       if (error) {
         console.log(error);
       }
     }
-  }
-}
-
-async function addUpvote(id) {
-  const { data, error } = await client.from("upvotes").insert([{ user_id: user_id, tool_id: id, user_email: user_email }]);
-  if (data) {
-    console.log(data);
-  }
-  if (error) {
-    console.log(error);
   }
 }
