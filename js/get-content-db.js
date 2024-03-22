18b7076e30118aa10e5cf664d0313541f42ef1b2
monzanifabio <renderbros@gmail.com>
2024-03-22 20:42:20 +0000

    checkSession hotfix

--- js/get-content-db.js
+++ js/get-content-db.js
@@ -23,7 +23,7 @@ async function getUserUpvotes(user_id) {
 }
 
 async function getContent() {
-  console.log(user_id);
+  await checkSession();
   if (user_id != null) {
     await getUserUpvotes(user_id);
   }
